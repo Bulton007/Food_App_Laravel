69b3b755dd988d6ab4b12234b9cc48bcaa35a4cb
Bulton007 <deluchb@gmail.com>
2026-03-29 00:54:38 +0700

    fix migration script

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,43 +15,25 @@ class DatabaseSeeder extends Seeder
     {
         // ✅ CREATE USER (VERY IMPORTANT)
         Profile::create([
-            'name' => 'Test User',
-            'email' => 'test@test.com',
-            'password' => bcrypt('123456'),
-            'image' => null
+        'name' => 'Test User',
+        'email' => 'test@test.com',
+        'password' => bcrypt('123456'),
+        'image' => null
+    ]);
+
+    // ✅ ONLY CATEGORIES
+    $categories = [
+        'Burger',
+        'Pizza',
+        'Spaghetti',
+        'Sandwich',
+        'Chicken Nugget'
+    ];
+
+    foreach ($categories as $catName) {
+        Category::create([
+            'name' => $catName
         ]);
-
-        // Categories
-        $categories = [
-            'Burger',
-            'Pizza',
-            'Spaghetti',
-            'Sandwich',
-            'Chicken Nugget'
-        ];
-
-        foreach ($categories as $catName) {
-            $category = Category::create([
-                'name' => $catName
-            ]);
-
-            for ($i = 1; $i <= 3; $i++) {
-
-                $product = Product::create([
-                    'title' => $catName . ' ' . $i,
-                    'subtitle' => 'Delicious ' . $catName,
-                    'price' => rand(5, 20),
-                    'rating' => rand(3, 5),
-                    'category_id' => $category->id
-                ]);
-
-                for ($j = 1; $j <= 5; $j++) {
-                    ProductImage::create([
-                        'product_id' => $product->id,
-                        'image_url' => 'https://picsum.photos/200?random=' . rand(1,1000)
-                    ]);
-                }
-            }
-        }
+    }
     }
 }
